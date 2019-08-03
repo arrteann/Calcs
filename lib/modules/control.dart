8e5673489fbd0825b01d7d8dec95100dbc39be4c
artin <artindev@gmail.com>
2019-08-04 02:14:57 +0430

    init project 2

---
+++ lib/modules/control.dart
@@ -0,0 +1,4 @@
+import 'package:calcs/main.dart';
+import 'package:calcs/modules/view.dart';
+import 'package:flutter/material.dart';
+
---
+++ lib/modules/view.dart
@@ -0,0 +1,5 @@
+import 'package:calcs/main.dart';
+import 'package:calcs/modules/control.dart';
+import 'package:flutter/material.dart';
+
+
